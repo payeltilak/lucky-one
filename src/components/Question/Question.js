9117511722626941d5578f43a08eb062f6b00fbb
unknown <you@example.com>
2022-03-26 21:52:26 +0600

    changes & added ans

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,15 +4,18 @@ const Question = () => {
     return (
         <div>
             <h3>How React Works?</h3>
-            <p>Ans : React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible.
-                It designs simple views for each state in your application.
-                React will efficiently update and render just the right component when your data change
+            <p>Ans : React uses a declarative paradigm that makes it easier to reason about your application. <br />Aims to be both efficient and flexible.<br />
+                It designs simple views for each state in your application.<br />
+                React will efficiently update and render just the right component when your data change.<br />
+                What you picture could look like the screen below, with a navbar, a header, a filter, and a list.
                 s</p><br />
             <h3>Props vs State..</h3>
-            <p>Ans : Props: Props are known as properties it can be used to pass data from one component to another.
+            <p>Ans : Props: Props are known as properties it can be used to pass data from one component to another.<br />
                 Props cannot be modified, read-only, and Immutable.<br />
+                Props can be used with state and functional components..
 
-                <p>State : The state represents parts of an Application that can change. Each component can have its State. The state is Mutable and It is local to the component only.</p>
+                <p>State : The state represents parts of an Application that can change.<br /> Each component can have its State. <br />The state is Mutable and It is local to the component only.<br /></p>
+                <p>State : The state represents parts of an Application that can change.<br /> Each component can have its State. <br />The state is Mutable and It is local to the component only.<br /></p>
 
             </p>
         </div>
